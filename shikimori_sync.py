8db0504b47eb9c3591bab7c50f64b08894751f15
Divarion-D <danil.dmitrievn@gmail.com>
2023-07-10 22:06:14 +0300

    upd

--- shikimori_sync.py
+++ shikimori_sync.py
@@ -1,29 +1,32 @@
-from email import header
-import requests
 import json
-import time
 import os
 import re
+import time
+
+import requests
 
 # Shikimori API
 # https://shikimori.one/api/doc/1.0
 
 USERNAME = "Divarion_D"
+HEADER = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0",
+}
 
 
 def WatchAnime(data):
     for i in data:
-        anime = requests.get(f"https://shikimori.one/api/animes/{i.get('anime').get('id')}", headers=header)
+        anime = requests.get(f"https://shikimori.one/api/animes/{i.get('anime').get('id')}", headers=HEADER)
         anime = json.loads(anime.text)
         desc = '' if anime.get('description') is None else anime.get('description')
         # remove [*] from description
         desc = re.sub(r'\[.*?\]', '', desc)
 
         if anime.get('kind') == 'movie':
-            ty_res = time.gmtime(anime.get('duration')*60)
-            time_m = time.strftime("%H:%M:%S",ty_res)
+            ty_res = time.gmtime(anime.get('duration') * 60)
+            time_m = time.strftime("%H:%M:%S", ty_res)
             data = {
-                "id" :  anime.get('id'),
+                "id": anime.get('id'),
                 "name": anime.get('russian'),
                 "originalName": anime.get('name'),
                 "date": anime.get('aired_on'),
@@ -34,7 +37,7 @@ def WatchAnime(data):
             }
         else:
             data = {
-                "id" :  anime.get('id'),
+                "id": anime.get('id'),
                 "name": anime.get('russian'),
                 "originalName": anime.get('name'),
                 "date": anime.get('aired_on'),
@@ -64,19 +67,24 @@ def WatchAnime(data):
                 json.dump(data_json, f, indent=4, ensure_ascii=False)
                 f.close()
 
+
 def Schuduled(data):
+    # remove sheduled.json
+    if os.path.exists(f'anime-data/schedule.json'):
+        os.remove(f'anime-data/schedule.json')
+
     for i in data:
-        anime = requests.get(f"https://shikimori.one/api/animes/{i.get('anime').get('id')}", headers=header)
+        anime = requests.get(f"https://shikimori.one/api/animes/{i.get('anime').get('id')}", headers=HEADER)
         anime = json.loads(anime.text)
         desc = '' if anime.get('description') is None else anime.get('description')
         # remove [*] from description
         desc = re.sub(r'\[.*?\]', '', desc)
 
         if anime.get('kind') == 'movie':
-            ty_res = time.gmtime(anime.get('duration')*60)
-            time_m = time.strftime("%H:%M:%S",ty_res)
+            ty_res = time.gmtime(anime.get('duration') * 60)
+            time_m = time.strftime("%H:%M:%S", ty_res)
             data = {
-                "id" :  anime.get('id'),
+                "id": anime.get('id'),
                 "name": anime.get('russian'),
                 "originalName": anime.get('name'),
                 "date": anime.get('aired_on'),
@@ -87,7 +95,7 @@ def Schuduled(data):
             }
         else:
             data = {
-                "id" :  anime.get('id'),
+                "id": anime.get('id'),
                 "name": anime.get('russian'),
                 "originalName": anime.get('name'),
                 "date": anime.get('aired_on'),
@@ -96,7 +104,6 @@ def Schuduled(data):
                 "img": f"https://shikimori.one{anime.get('image').get('original')}",
                 "description": desc,
             }
-
 
         if os.path.exists(f'anime-data/schedule.json'): 
             with open(f'anime-data/schedule.json', 'r') as f:
@@ -116,16 +123,12 @@ def Schuduled(data):
                 json.dump(data_json, f, indent=4, ensure_ascii=False)
                 f.close()
 
+
 if __name__ == '__main__':
-
-    header = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0",
-    }
-
-    data = requests.get(f"https://shikimori.one/api/users/{USERNAME}/anime_rates?limit=1000000000&status=completed", headers=header)
+    data = requests.get(f"https://shikimori.one/api/users/{USERNAME}/anime_rates?limit=1000000000&status=completed", headers=HEADER)
     data = json.loads(data.text)
-    #WatchAnime(data)
+    WatchAnime(data)
 
-    data = requests.get(f"https://shikimori.one/api/users/{USERNAME}/anime_rates?limit=1000000000&status=planned", headers=header)
+    data = requests.get(f"https://shikimori.one/api/users/{USERNAME}/anime_rates?limit=1000000000&status=planned", headers=HEADER)
     data = json.loads(data.text)
     Schuduled(data)
